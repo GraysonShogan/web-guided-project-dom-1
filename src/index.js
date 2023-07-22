07892c768cce44beb9dcab9634ba5b72b94438ac
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-22 12:04:06 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -8,27 +8,44 @@ const header = document.querySelector("header");
 const logoTitle = document.querySelector("#logoTitle");
 const firstCard = document.querySelector(".card");
 // B- finding within one particular element
-const imageFirstCard = null;
-const titleFirstCard = null;
-const subtitleFirstCard = null;
-const textFirstCard = null;
+const imageFirstCard = firstCard.querySelector("img");
+const titleFirstCard = firstCard.querySelector(".card-title");
+const subtitleFirstCard = firstCard.querySelector(".card-subtitle");
+const textFirstCard = firstCard.querySelector(".card-text");
 // C- traversing with dot notation
-const link1FirstCard = null;
-const link2FirstCard = null;
+const link1FirstCard = textFirstCard.nextElementSibling;
+const link2FirstCard = link1FirstCard.nextElementSibling;
 
 // 👉 2- Finding collections of elements in the DOM
 // A- Find all the anchor tags inside the nav element
+const links = document.querySelectorAll("nav a");
 // B- Loop over the links and console.log their text content
+links.forEach((link) => console.log(link.textContent)); // higher order function => function that takes another function as an argument
 // C- Turn the collection of links into a real array
+const linksArr = Array.from(links);
 // D- Use .filter to find the anchor tag with the textContent of "Home"
+linksArr.find((link) => link.textContent === "Home");
+
+// find => returns the first element which passes the CSSConditionRule, meets the criteria
+// filter => returns all elements which pass the condition
+// map => make some update to all elements of the Array
+// reduce => reduce the array to a smaller value
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
 //  B- Have the students research online the difference between textContent and innerText
-
+logoTitle.textContent = "Bloomtech Dogs";
+titleFirstCard.textContent = "I am a dog";
+subtitleFirstCard.textContent = "A story in three parts";
+textFirstCard.textContent = "lorem ipsum or whateva";
+link2FirstCard.textContent = "Dog ipsum";
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
+logoTitle.className = "logo heading banana";
+link2FirstCard.href = "https://doggoipsum.com";
+imageFirstCard.src = "https://dogpictures.com";
 //  B- Using .setAttribute to change a few attributes
+link1FirstCard.setAttribute("href", "https://pexels.com/search/dog");
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
